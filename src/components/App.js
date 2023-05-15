7e98fa4504102fbbf870b1b9b30c2b2cdb1ba2a9
Gaurav Soni <gsoni8158@gmail.com>
2023-05-15 18:14:02 +0530

    submitting code-Mon May 15 2023 18:14:02 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
-import React from "react";
-
+import React from 'react';
 
 const App = () => {
-    return (
-        <div>
-            <p>you can now create full fleged React components and render it on the DOM node of your choice.</p>
-        </div>
-      )
-  }
-  
+  return (
+    <div>
+      <p>I can now create full fleged React components and render it on the DOM node</p>
+    </div>
+  );
+};
 
-export default App
+export default App;
